5aade20ba2fde584b94a42a89217a591572e78c3
nbur4556 <nbur4556@gmail.com>
2021-01-19 19:32:01 -0600

    feat: send main client file for all html server requests
    Adds if-env to read environment variable, and determine production and development start scripts. Adds concurrently to react page to proxy to server when running in developer mode.

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = 3001;
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(path.resolve(), './client/build/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);

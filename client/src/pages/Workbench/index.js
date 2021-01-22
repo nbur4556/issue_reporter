267765395db61d21d5f24bd373cbf644e0a283b5
nbur4556 <nbur4556@gmail.com>
2021-01-22 17:44:16 -0600

    feat: create workbench page

---
+++ client/src/pages/Workbench/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Workbench = () => {
+    return (
+        <article>
+            <h1>Workbench Page</h1>
+        </article>
+    );
+}
+
+export default Workbench;

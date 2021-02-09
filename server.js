5a252aa4991197ac4afe9d1104105b2ca30d6bdc
nbur4556 <nbur4556@gmail.com>
2021-02-08 23:53:35 -0600

    fix: establish connection to mongo db uri using env variables

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
-const PORT = 3001;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Database
-mongoose.connect('URI', {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true
-}, dbConnection => {
-    console.log(dbConnection);
+// Connect to Database
+mongoose.connect(
+    process.env.DB_URI,
+    { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+).then(({ connections }) => {
+    console.log(`Connected on port ${connections[0].port}`);
 });
 
 // Routes
@@ -25,6 +25,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(path.resolve(), './client/build/index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}...`);
 });

861f8377e3b35629644d8b92720b8bc65e46c85a
nbur4556 <nbur4556@gmail.com>
2021-02-12 15:14:31 -0600

    feat: create issue controller

---
+++ controllers/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    issueController: require('./issueController.js')
+}
---
+++ controllers/issueController.js
@@ -0,0 +1,41 @@
+const db = require('../models');
+
+module.exports = {
+    // Read Issue
+    findById: function (searchId) {
+        db.Issue.findOne({ _id: searchId }, (err, result) => {
+            if (err) { console.log(err); }
+            if (result) { console.log(result); }
+        });
+    },
+
+    // Create Issue
+    create: function (issueParams) {
+        db.Issue.create({
+            name: issueParams.name,
+            body: issueParams.body
+        }, (err, result) => {
+            if (err) { console.log(err); }
+            if (result) { console.log(result); }
+        });
+    },
+
+    // Update Issue
+    updateById: function (searchId, issueParams) {
+        db.Issue.updateOne({ _id: searchId }, {
+            name: issueParams.name,
+            body: issueParams.body
+        }, (err, result) => {
+            if (err) { console.log(err) };
+            if (result) { console.log(result); }
+        })
+    },
+
+    // Delete Issue
+    deleteById: function (searchId) {
+        db.Issue.deleteOne({ _id: searchId }, (err, result) => {
+            if (err) { console.log(err); }
+            if (result) { console.log(result); }
+        })
+    }
+}

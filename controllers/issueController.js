4166eb815a55c02c90cfefb0a5ac498f69825868
nbur4556 <nbur4556@gmail.com>
2021-02-12 15:58:22 -0600

    fix: do not update issue value if change parameter does not exist

--- controllers/issueController.js
+++ controllers/issueController.js
@@ -1,9 +1,11 @@
+const { response } = require('express');
 const db = require('../models');
 
 module.exports = {
     // Read Issue
     findById: function (searchId, cb) {
-        db.Issue.findOne({ _id: searchId }, (err, result) => (err) ? cb(err) : cb(result));
+        db.Issue.findOne({ _id: searchId },
+            (err, result) => (err) ? cb(err) : cb(result));
     },
 
     // Create Issue
@@ -11,19 +13,30 @@ module.exports = {
         db.Issue.create({
             name: issueParams.name,
             body: issueParams.body
-        }, (err, result) => (err) ? cb(err) : cb(result));
+        },
+            (err, result) => (err) ? cb(err) : cb(result));
     },
 
     // Update Issue
     updateById: function (searchId, issueParams, cb) {
-        db.Issue.updateOne({ _id: searchId }, {
-            name: issueParams.name,
-            body: issueParams.body
-        }, (err, result) => (err) ? cb(err) : cb(result));
+        db.Issue.findOne({ _id: searchId },
+            (err, resultIssue) => {
+                if (err) { cb(err) }
+
+                db.Issue.updateOne({ _id: searchId },
+                    {
+                        $set: {
+                            name: issueParams.name || resultIssue.name,
+                            body: issueParams.body || resultIssue.body
+                        }
+                    },
+                    (err, result) => (err) ? cb(err) : cb(result));
+            });
     },
 
     // Delete Issue
     deleteById: function (searchId, cb) {
-        db.Issue.deleteOne({ _id: searchId }, (err, result) => (err) ? cb(err) : cb(result));
+        db.Issue.deleteOne({ _id: searchId },
+            (err, result) => (err) ? cb(err) : cb(result));
     }
 }

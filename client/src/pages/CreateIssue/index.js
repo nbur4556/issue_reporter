6cc29436d1cc6d338b30e36ff7e9b6f0f3f45ad0
nbur4556 <nbur4556@gmail.com>
2021-02-22 17:43:23 -0600

    feat: start create issue page

---
+++ client/src/pages/CreateIssue/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const CreateIssue = () => {
+    return (
+        <article>
+            Create Issue
+        </article>
+    );
+}
+
+export default CreateIssue;

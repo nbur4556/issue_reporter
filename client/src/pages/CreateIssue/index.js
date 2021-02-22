5045f84f63ae8b9c341daed3a18d9d9d489f2e55
nbur4556 <nbur4556@gmail.com>
2021-02-22 17:55:29 -0600

    feat: create input form for new issues

--- client/src/pages/CreateIssue/index.js
+++ client/src/pages/CreateIssue/index.js
@@ -4,7 +4,26 @@ import './style.css';
 const CreateIssue = () => {
     return (
         <article>
-            Create Issue
+
+            {/* Input Form */}
+
+            <form>
+                <label>Name</label>
+                <input type="text" />
+                <label>Body</label>
+                <input type="text" />
+                <label>Category</label>
+                <input type="text" />
+                <label>Assigned</label>
+                <input type="text" />
+                <label>Due Date</label>
+                <input type="text" />
+                <label>Comments</label>
+                <input type="text" />
+                <label>Status</label>
+                <input type="text" />
+            </form>
+
         </article>
     );
 }

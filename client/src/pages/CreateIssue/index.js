9bb686ddcdc7e8b5db71262c6fcf6ff1876cb169
nbur4556 <nbur4556@gmail.com>
2021-02-22 18:21:30 -0600

    feat: set all form input data in state

--- client/src/pages/CreateIssue/index.js
+++ client/src/pages/CreateIssue/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 const CreateIssue = () => {
+    const [issueData, setIssueData] = useState({
+        name: '',
+        body: '',
+        category: '',
+        assigned: '',
+        dueDate: '',
+        comments: '',
+        status: ''
+    });
 
+    // Set issue data state for form inputs
+    const handleSetIssueData = e => {
+        const input = e.currentTarget;
+        setIssueData({ ...issueData, [input.name]: input.value });
+    }
+
+    // Create an issue on submit
     const handleCreateIssue = e => {
         e.preventDefault();
-        const createIssueForm = e.currentTarget.parentElement;
 
-        console.log(createIssueForm);
+        console.log(issueData);
     }
 
     return (
@@ -17,26 +32,40 @@ const CreateIssue = () => {
 
             <form>
 
-                <label htmlFor="name">Name</label>
-                <input id="name" name="name" type="text" />
+                <label htmlFor="name">
+                    Name:
+                    <input id="name" name="name" type="text" onChange={handleSetIssueData} />
+                </label>
 
-                <label htmlFor="body">Body</label>
-                <input id="body" name="body" type="text" />
+                <label htmlFor="body">
+                    Body:
+                    <input id="body" name="body" type="text" onChange={handleSetIssueData} />
+                </label>
 
-                <label htmlFor="category">Category</label>
-                <input id="category" name="category" type="text" />
+                <label htmlFor="category">
+                    Category:
+                    <input id="category" name="category" type="text" onChange={handleSetIssueData} />
+                </label>
 
-                <label htmlFor="assigned">Assigned</label>
-                <input id="assigned" name="assigned" type="text" />
+                <label htmlFor="assigned">
+                    Assigned:
+                    <input id="assigned" name="assigned" type="text" onChange={handleSetIssueData} />
+                </label>
 
-                <label htmlFor="dueDate">Due Date</label>
-                <input id="dueDate" name="dueDate" type="text" />
+                <label htmlFor="dueDate">
+                    Due Date:
+                    <input id="dueDate" name="dueDate" type="text" onChange={handleSetIssueData} />
+                </label>
 
-                <label htmlFor="comments">Comments</label>
-                <input id="comments" name="comments" type="text" />
+                <label htmlFor="comments">
+                    Comments:
+                    <input id="comments" name="comments" type="text" onChange={handleSetIssueData} />
+                </label>
 
-                <label htmlFor="status">Status</label>
-                <input id="status" name="status" type="text" />
+                <label htmlFor="status">
+                    Status:
+                    <input id="status" name="status" type="text" onChange={handleSetIssueData} />
+                </label>
 
                 <button onClick={handleCreateIssue}>Submit</button>
 

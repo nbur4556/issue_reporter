740c7d8b6f2c273c6fc58a21e46e06218a954dd3
nbur4556 <nbur4556@gmail.com>
2021-02-24 18:34:59 -0600

    feat: add route to find a project by id

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,3 +1,24 @@
 const db = require('../models');
 
-module.exports = {}
+module.exports = {
+    // Read Project
+    findById: function (searchId, cb) {
+        db.Project.findOne({ _id: searchId },
+            (err, result) => (err) ? cb(err) : cb(result));
+    },
+
+    // Create Project
+    create: function () {
+        console.log('placeholder')
+    },
+
+    // Update Project
+    updateById: function () {
+        console.log('placeholder')
+    },
+
+    // Delete Project
+    deleteById: function () {
+        console.log('placeholder')
+    }
+}
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,47 @@
 const controllers = require('../controllers');
 
 module.exports = function (app) {
+    // User Routes
+    app.get('/api/user/:searchId', (req, res) => {
+        controllers.userController.findById(req.params.searchId, (result) => {
+            (result?.errors)
+                ? res.status(400).json(result.errors)
+                : res.status(200).json(result);
+        });
+    });
+
+    app.post('/api/user', (req, res) => {
+        controllers.userController.create(req.body, (result) => {
+            (result.errors)
+                ? res.status(400).json(result.errors)
+                : res.status(200).json(result);
+        });
+    });
+
+    app.put('/api/user/:searchId', (req, res) => {
+        controllers.userController.updateById(req.params.searchId, req.body, result => {
+            (result.errors)
+                ? res.status(400).json(result.errors)
+                : res.status(200).json(result);
+        });
+    });
+
+    app.delete('/api/user/:searchId', (req, res) => {
+        controllers.userController.deleteById(req.params.searchId, (result) => {
+            (result.errors)
+                ? res.status(400).json(result.errors)
+                : res.status(200).json(result);
+        });
+    });
+
+    // Project Routes
+    app.get('/api/project/:searchId', (req, res) => {
+        controllers.projectController.findById(req.params.searchId,
+            (result) => (result.errors)
+                ? res.status(400).json(result.errors)
+                : res.status(200).json(result));
+    });
+
     // Issue Routes
     app.get('/api/issue', (req, res) => {
         controllers.issueController.find((result) => {
@@ -37,39 +78,6 @@ module.exports = function (app) {
     app.delete('/api/issue/:searchId', (req, res) => {
         controllers.issueController.deleteById(req.params.searchId, (result) => {
             (result?.errors)
-                ? res.status(400).json(result.errors)
-                : res.status(200).json(result);
-        });
-    });
-
-    // User Routes
-    app.get('/api/user/:searchId', (req, res) => {
-        controllers.userController.findById(req.params.searchId, (result) => {
-            (result?.errors)
-                ? res.status(400).json(result.errors)
-                : res.status(200).json(result);
-        });
-    });
-
-    app.post('/api/user', (req, res) => {
-        controllers.userController.create(req.body, (result) => {
-            (result.errors)
-                ? res.status(400).json(result.errors)
-                : res.status(200).json(result);
-        });
-    });
-
-    app.put('/api/user/:searchId', (req, res) => {
-        controllers.userController.updateById(req.params.searchId, req.body, result => {
-            (result.errors)
-                ? res.status(400).json(result.errors)
-                : res.status(200).json(result);
-        });
-    });
-
-    app.delete('/api/user/:searchId', (req, res) => {
-        controllers.userController.deleteById(req.params.searchId, (result) => {
-            (result.errors)
                 ? res.status(400).json(result.errors)
                 : res.status(200).json(result);
         });

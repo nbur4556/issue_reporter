8ff00e20afa627e0cf73d5d5afa2c19679e6d8ad
nbur4556 <nbur4556@gmail.com>
2021-02-28 18:35:58 -0600

    test: create issue tests delete the tests after they are created

--- cypress/integration/issues.spec.js
+++ cypress/integration/issues.spec.js
@@ -6,68 +6,107 @@ const dueDate = '2021-02-27';
 // Create Issue Tests
 describe('Create issue', () => {
     const successMessage = `Success! Issue "${issueName}" created.`;
+    let testId;
 
     beforeEach(() => {
         cy.visit('/create-issue');
+        cy.intercept('/api/issue').as('issueData');
+    });
+
+    afterEach(() => {
+        cy.request('DELETE', `/api/issue/${testId}`);
     });
 
     // Check that form with all information creates success message
-    // it('create issue with all information', () => {
-    //     cy.get('input[name="name"]').type(issueName);
-    //     cy.get('textarea[name="body"]').type(description);
-    //     cy.get('select[name="category"]').select(category);
-    //     cy.get('input[name="dueDate"]').type(dueDate);
+    it('create issue with all information', () => {
+        cy.get('input[name="name"]').type(issueName);
+        cy.get('textarea[name="body"]').type(description);
+        cy.get('select[name="category"]').select(category);
+        cy.get('input[name="dueDate"]').type(dueDate);
 
-    //     cy.get('button[name="submit"]').click().then((data) => {
-    //         console.log(data);
-    //     });
+        cy.get('button[name="submit"]')
+            .click()
+            .wait('@issueData')
+            .then((xhr) => {
+                testId = xhr.response.body._id;
+            });
 
-    //     cy.contains(successMessage).should('exist');
-    // });
+        cy.contains(successMessage).should('exist');
+    });
 
-    // // Check that form without required information creates success message
-    // it('create issue without body', () => {
-    //     cy.get('input[name="name"]').type(name);
-    //     cy.get('select[name="category"]').select(category);
-    //     cy.get('input[name="dueDate"]').type(dueDate);
+    // Check that form without required information creates success message
+    it('create issue without body', () => {
+        cy.get('input[name="name"]').type(issueName);
+        cy.get('select[name="category"]').select(category);
+        cy.get('input[name="dueDate"]').type(dueDate);
 
-    //     cy.get('button[name="submit"]').click();
-    //     cy.contains(successMessage).should('exist');
-    // });
+        cy.get('button[name="submit"]')
+            .click()
+            .wait('@issueData')
+            .then((xhr) => {
+                testId = xhr.response.body._id;
+            });
 
-    // it('create issue without category', () => {
-    //     cy.get('input[name="name"]').type(name);
-    //     cy.get('textarea[name="body"]').type(description);
-    //     cy.get('input[name="dueDate"]').type(dueDate);
+        cy.contains(successMessage).should('exist');
+    });
 
-    //     cy.get('button[name="submit"]').click();
-    //     cy.contains(successMessage).should('exist');
-    // });
+    it('create issue without category', () => {
+        cy.get('input[name="name"]').type(issueName);
+        cy.get('textarea[name="body"]').type(description);
+        cy.get('input[name="dueDate"]').type(dueDate);
 
-    // it('create issue without due date', () => {
-    //     cy.get('input[name="name"]').type(name);
-    //     cy.get('textarea[name="body"]').type(description);
-    //     cy.get('select[name="category"]').select(category);
+        cy.get('button[name="submit"]')
+            .click()
+            .wait('@issueData')
+            .then((xhr) => {
+                testId = xhr.response.body._id;
+            });
 
-    //     cy.get('button[name="submit"]').click();
-    //     cy.contains(successMessage).should('exist');
-    // });
+        cy.contains(successMessage).should('exist');
+    });
 
-    // // Check that form without name does not create success message
-    // it('submit form without name does not create issue', () => {
-    //     cy.get('textarea[name="body"]').type(description);
-    //     cy.get('select[name="category"]').select(category);
-    //     cy.get('input[name="dueDate"]').type(dueDate);
+    it('create issue without due date', () => {
+        cy.get('input[name="name"]').type(issueName);
+        cy.get('textarea[name="body"]').type(description);
+        cy.get('select[name="category"]').select(category);
 
-    //     cy.get('button[name="submit"]').click();
-    //     cy.contains(successMessage).should('not.exist');
-    // });
+        cy.get('button[name="submit"]')
+            .click()
+            .wait('@issueData')
+            .then((xhr) => {
+                testId = xhr.response.body._id;
+            });
 
-    // // Check that empty form does not create success message
-    // it('submit empty form does not create issue', () => {
-    //     cy.get('button[name="submit"]').click();
-    //     cy.contains(successMessage).should('not.exist');
-    // });
+        cy.contains(successMessage).should('exist');
+    });
+
+    // Check that form without name does not create success message
+    it('submit form without name does not create issue', () => {
+        cy.get('textarea[name="body"]').type(description);
+        cy.get('select[name="category"]').select(category);
+        cy.get('input[name="dueDate"]').type(dueDate);
+
+        cy.get('button[name="submit"]')
+            .click()
+            .wait('@issueData')
+            .then((xhr) => {
+                testId = xhr.response.body._id;
+            });
+
+        cy.contains(successMessage).should('not.exist');
+    });
+
+    // Check that empty form does not create success message
+    it('submit empty form does not create issue', () => {
+        cy.get('button[name="submit"]')
+            .click()
+            .wait('@issueData')
+            .then((xhr) => {
+                testId = xhr.response.body._id;
+            });
+
+        cy.contains(successMessage).should('not.exist');
+    });
 });
 
 // Update Issue Tests
@@ -115,5 +154,5 @@ describe('Delete Issue', () => {
         cy.get('button[name="cancelDelete"]').should('not.exist');
 
         cy.get('.issueListSection').contains(issueName).should('not.exist');
-    })
+    });
 });

b118425bb0537f34ed5f10adad840bdf19d77087
nbur4556 <nbur4556@gmail.com>
2021-03-05 18:02:58 -0600

    feat: define binary tree node

---
+++ client/src/utils/treeSort.js
@@ -0,0 +1,23 @@
+function Node(value) {
+    this.value = value;
+    this.leftNode = null;
+    this.rightNode = null;
+}
+
+function treeSort(data, sortParam) {
+
+    // Build binary tree
+
+    // set root node
+    const rootNode = new Node(0);
+    console.log(rootNode);
+    // set smaller left
+    // set larger right
+    // repeat
+
+
+    // Read binary tree
+
+}
+
+treeSort();

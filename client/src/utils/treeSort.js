3379e28b6ea11911d8772e4b1900bcaa35767e2a
nbur4556 <nbur4556@gmail.com>
2021-03-05 18:39:17 -0600

    fix: add multiple nodes using recursion to create binary tree

--- client/src/utils/treeSort.js
+++ client/src/utils/treeSort.js
@@ -9,10 +9,14 @@ function BinaryTree(rootNode) {
 
     this.addTo = (node, parentNode = this.rootNode) => {
         if (node.value <= parentNode.value) {
-            parentNode.leftNode = node;
+            (parentNode.leftNode === null)
+                ? parentNode.leftNode = node
+                : this.addTo(node, parentNode.leftNode);
         }
         else {
-            parentNode.rightNode = node;
+            (parentNode.rightNode === null)
+                ? parentNode.rightNode = node
+                : this.addTo(node, parentNode.rightNode);
         }
     }
 }
@@ -30,7 +34,14 @@ function treeSort(data, sortParam) {
     const leftNode = new Node(data[2]);
     tree.addTo(leftNode);
 
+    const largestNode = new Node(data[3]);
+    tree.addTo(largestNode);
+
+    const smallestNode = new Node(data[4]);
+    tree.addTo(smallestNode);
+
+
     console.log(tree);
 }
 
-treeSort([5, 7, 4, 321, 9874, 564, 651, 35, 574, 684, 351, 74, 684, 351, 1], 'sortParam');
+treeSort([5, 5, 4, 321, 1, 564, 651, 35, 574, 684, 351, 74, 684, 351, 1], 'sortParam');

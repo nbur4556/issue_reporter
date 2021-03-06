94598a7b9766b2e1361f3bc4dfe76575f880aa1a
nbur4556 <nbur4556@gmail.com>
2021-03-05 18:35:29 -0600

    feat: add single nodes to root node of tree

--- client/src/utils/treeSort.js
+++ client/src/utils/treeSort.js
@@ -4,20 +4,33 @@ function Node(value) {
     this.rightNode = null;
 }
 
+function BinaryTree(rootNode) {
+    this.rootNode = rootNode;
+
+    this.addTo = (node, parentNode = this.rootNode) => {
+        if (node.value <= parentNode.value) {
+            parentNode.leftNode = node;
+        }
+        else {
+            parentNode.rightNode = node;
+        }
+    }
+}
+
 function treeSort(data, sortParam) {
 
     // Build binary tree
 
-    // set root node
-    const rootNode = new Node(0);
-    console.log(rootNode);
-    // set smaller left
-    // set larger right
-    // repeat
+    const rootNode = new Node(data[0]);
+    const tree = new BinaryTree(rootNode);
 
+    const rightNode = new Node(data[1]);
+    tree.addTo(rightNode);
 
-    // Read binary tree
+    const leftNode = new Node(data[2]);
+    tree.addTo(leftNode);
 
+    console.log(tree);
 }
 
-treeSort();
+treeSort([5, 7, 4, 321, 9874, 564, 651, 35, 574, 684, 351, 74, 684, 351, 1], 'sortParam');

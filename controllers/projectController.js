c1d283a2481a02fe02bb8b8274352ec5032fa863
nbur4556 <nbur4556@gmail.com>
2021-03-10 09:41:49 -0600

    fix: return mongoose promise instead of new promise for all project methods

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -3,10 +3,7 @@ const db = require('../models');
 module.exports = {
     // Read Project
     findById: function (searchId) {
-        return new Promise((resolve, reject) => {
-            db.Project.findOne({ _id: searchId },
-                (err, result) => (err) ? reject(err) : resolve(result));
-        });
+        return db.Project.findOne({ _id: searchId }).exec();
     },
 
     // Create Project
@@ -16,17 +13,11 @@ module.exports = {
 
     // Update Project
     updateById: function (searchId, projectParams) {
-        return new Promise((resolve, reject) => {
-            db.Project.updateOne({ _id: searchId }, { $set: { ...projectParams } },
-                (err, result) => (err) ? reject(err) : resolve(result));
-        });
+        return db.Project.updateOne({ _id: searchId }, { $set: { ...projectParams } });
     },
 
     // Delete Project
     deleteById: function (searchId) {
-        return new Promise((resolve, reject) => {
-            db.Project.deleteOne({ _id: searchId },
-                (err, result) => (err) ? resolve(err) : reject(result));
-        });
+        return db.Project.deleteOne({ _id: searchId });
     }
 }

b929fb07d175342a5ed7e0afdaef828e1ab687f7
nbur4556 <nbur4556@gmail.com>
2021-03-18 18:15:00 -0500

    ref: render project list items in seperate function for project manager

--- client/src/components/ProjectManager/index.js
+++ client/src/components/ProjectManager/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ProjectManager = (props) => {
+    const [editState, setEditState] = useState(false);
+
+    const renderProjects = (projectsList) => {
+        const projectListItems = projectsList.map((project, index) => {
+            return (
+                <li key={index} data-projectid={project._id}>
+                    {project.projectName}
+                    <button onClick={props.addTab}>Add Tab</button>
+                    <button>Edit Project</button>
+                    <button onClick={props.deleteProject}>Delete Project</button>
+                </li>
+            );
+        });
+
+        return projectListItems;
+    }
+
     return (
         <ul>
-            {props.projects.map((project, index) => {
-                return (
-                    <li key={index} data-projectid={project._id}>
-                        {project.projectName}
-                        <button onClick={props.addTab}>Add Tab</button>
-                        <button onClick={props.deleteProject}>Delete Project</button>
-                    </li>
-                );
-            })}
-
-            <p>Project Manager</p>
+            {(editState) ? null : renderProjects(props.projects)}
         </ul>
     )
 }

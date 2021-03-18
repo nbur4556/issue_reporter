a8143fb4a3eb48f28d3d9d0fc65483c0361573e9
nbur4556 <nbur4556@gmail.com>
2021-03-18 18:18:01 -0500

    feat: toggle project manager edit state method

--- client/src/components/ProjectManager/index.js
+++ client/src/components/ProjectManager/index.js
@@ -3,13 +3,17 @@ import React, { useState } from 'react';
 const ProjectManager = (props) => {
     const [editState, setEditState] = useState(false);
 
+    const toggleEditState = () => {
+        (editState) ? setEditState(false) : setEditState(true);
+    }
+
     const renderProjects = (projectsList) => {
         const projectListItems = projectsList.map((project, index) => {
             return (
                 <li key={index} data-projectid={project._id}>
                     {project.projectName}
                     <button onClick={props.addTab}>Add Tab</button>
-                    <button>Edit Project</button>
+                    <button onClick={toggleEditState}>Edit Project</button>
                     <button onClick={props.deleteProject}>Delete Project</button>
                 </li>
             );
@@ -18,9 +22,13 @@ const ProjectManager = (props) => {
         return projectListItems;
     }
 
+    const editProjects = () => {
+        return <p onClick={toggleEditState}>Edit Projects</p>
+    }
+
     return (
         <ul>
-            {(editState) ? null : renderProjects(props.projects)}
+            {(editState) ? editProjects() : renderProjects(props.projects)}
         </ul>
     )
 }

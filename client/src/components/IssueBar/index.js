258af271f06efd2d5b056ceece8110a66d437af0
nbur4556 <nbur4556@gmail.com>
2021-04-03 19:03:41 -0500

    feat: display formatted due dates for issues

--- client/src/components/IssueBar/index.js
+++ client/src/components/IssueBar/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import { format } from 'date-fns';
 
 const IssueBar = props => {
+    const { issueData } = props;
+
+    const formatDueDate = (dueDate) => {
+        if (!dueDate) {
+            return null;
+        }
+
+        const dateArray = dueDate.split('-');
+        return `${dateArray[1]}/${dateArray[2].split('T')[0]}/${dateArray[0]}`;
+    }
+
     return (
         <div
             className={`issue-bar ${props.activeClassName}`}
@@ -8,9 +20,9 @@ const IssueBar = props => {
             data-index={props.index}>
 
             <ul>
-                <li className="name-col">{props.issueData.name}</li>
-                <li className="category-col">{props.issueData.category}</li>
-                <li className="due-date-col">{props.issueData.dueDate}</li>
+                <li className="name-col">{issueData.name}</li>
+                <li className="category-col">{issueData.category}</li>
+                <li className="due-date-col">{formatDueDate(issueData.dueDate)}</li>
                 {(props.assigned) ? <li>{props.assigned}</li> : null}
             </ul>
 

17fcf8bb17322d04102c42b0cb725561ebb515ee
nbur4556 <nbur4556@gmail.com>
2021-04-06 18:46:21 -0500

    ref: remove unnecessary div tag from issue bar and issue list header components

--- client/src/components/IssueListHeader/index.js
+++ client/src/components/IssueListHeader/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 const IssueListHeader = () => {
     return (
-        <div className="issue-bar">
-            <ul>
-                <li className="name-col">Name</li>
-                <li className="category-col">Category</li>
-                <li className="due-date-col">Due Date</li>
-            </ul>
-        </div>
+        <ul className="issue-bar">
+            <li className="name-col">Name</li>
+            <li className="category-col">Category</li>
+            <li className="due-date-col">Due Date</li>
+        </ul>
     );
 }
 

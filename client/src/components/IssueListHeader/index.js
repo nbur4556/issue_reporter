485f3f995d96050a160559de10a44044499b9d1b
nbur4556 <nbur4556@gmail.com>
2021-04-06 18:49:48 -0500

    style: style issue list headers as headers

--- client/src/components/IssueListHeader/index.js
+++ client/src/components/IssueListHeader/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 const IssueListHeader = () => {
     return (
         <ul className="issue-bar">
-            <li className="name-col">Name</li>
-            <li className="category-col">Category</li>
-            <li className="due-date-col">Due Date</li>
+            <li className="name-col"><h4>Name</h4></li>
+            <li className="category-col"><h4>Category</h4></li>
+            <li className="due-date-col"><h4>Due Date</h4></li>
         </ul>
     );
 }

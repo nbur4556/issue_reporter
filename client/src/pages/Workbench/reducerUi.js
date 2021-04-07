64c2a64c87804ca38e5b577f3da3664ec98d87aa
nbur4556 <nbur4556@gmail.com>
2021-04-07 18:03:26 -0500

    feat: create a reducer ui function for use in workbench page

---
+++ client/src/pages/Workbench/reducerUi.js
@@ -0,0 +1,12 @@
+const reducerUi = (state, action) => {
+    switch (action.type) {
+        case 'case1':
+            return state;
+        case 'case2':
+            return state;
+        default:
+            return state
+    }
+}
+
+export default reducerUi;

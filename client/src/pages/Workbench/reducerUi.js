b74b5f1b0a4c0607c021f996a646e264f2e3ce09
nbur4556 <nbur4556@gmail.com>
2021-04-07 18:13:35 -0500

    feat: include available actions to reducer ui function

--- client/src/pages/Workbench/reducerUi.js
+++ client/src/pages/Workbench/reducerUi.js
@@ -1,11 +1,31 @@
+const ACTIONS = {
+    TOGGLE_PROJECT_MANAGER: 'toggle_project_manager',
+    TOGGLE_CREATE_ISSUE: 'toggle_create_issue',
+    ADD_PROJECT_TAB: 'add_project_tab',
+    REMOVE_PROJECT_TAB: 'remove_project_tab',
+    SELECT_PROJECT: 'select_project',
+    SELECT_ISSUE: 'select_issue',
+    DISPLAY_CLOSED_ISSUES: 'display_closed_issues'
+}
+
 const reducerUi = (state, action) => {
     switch (action.type) {
-        case 'case1':
+        case ACTIONS.TOGGLE_PROJECT_MANAGER:
             return state;
-        case 'case2':
+        case ACTIONS.TOGGLE_CREATE_ISSUE:
+            return state;
+        case ACTIONS.ADD_PROJECT_TAB:
+            return state;
+        case ACTIONS.REMOVE_PROJECT_TAB:
+            return state;
+        case ACTIONS.SELECT_PROJECT:
+            return state;
+        case ACTIONS.SELECT_ISSUE:
+            return state
+        case ACTIONS.DISPLAY_CLOSED_ISSUES:
             return state;
         default:
-            return state
+            return state;
     }
 }
 

10234d889cb9ed777dcc742cabd69ef75b9f073a
nbur4556 <nbur4556@gmail.com>
2021-04-11 18:26:20 -0500

    fix: get selected issue in handle set issue status method of issue interface

--- client/src/pages/Workbench/IssueInterface.js
+++ client/src/pages/Workbench/IssueInterface.js
@@ -2,6 +2,14 @@
 import ApiConnection from '../../utils/ApiConnection.js';
 const issueConnection = new ApiConnection('/api/issue');
 
+const getSelectIssue = (issueList, selectIssue) => {
+    for (const issue of issueList) {
+        if (issue._id === selectIssue) {
+            return issue;
+        }
+    }
+}
+
 const IssueInterface = ({ userData, userDataDispatcher, ui, uiDispatcher }) => {
     const handleLoadIssues = () => {
         if (!ui.selectProject) { return }
@@ -16,7 +24,7 @@ const IssueInterface = ({ userData, userDataDispatcher, ui, uiDispatcher }) => {
     }
 
     const handleSetIssueStatus = () => {
-        const { isOpen, _id: issueId } = userData.issueList[ui.selectIssue];
+        const { isOpen, _id: issueId } = getSelectIssue(userData.issueList, ui.selectIssue);
 
         // Deselect issue when closed and displaying closed issues is set to false
         if (isOpen === true && ui.displayClosedIssue === false)

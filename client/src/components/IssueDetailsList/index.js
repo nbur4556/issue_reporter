beddb57db7303f58e1d23aba164baa53c19eab54
nbur4556 <nbur4556@gmail.com>
2021-04-13 19:40:40 -0500

    ref: create seperate component for issue details list

---
+++ client/src/components/IssueDetailsList/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+// Components
+import DeleteConfirmation from '../DeleteConfirmation';
+
+const IssueDetailsList = (props) => {
+    const { issue, handleDeleteIssue, removeDeleteConfirmation, displayDeleteMsg, setDisplayDeleteMsg } = props;
+
+    return (
+        <ul>
+            {/* Display details if details are available */}
+
+            {(issue.name) ? <li className="list-content">
+                {`Name: ${issue.name}`}
+            </li> : null}
+            {(issue.body) ? <li className="list-content">
+                {`Body: ${issue.body}`}
+            </li> : null}
+            {(issue.category) ? <li className="list-content">
+                {`Category: ${issue.category}`}
+            </li> : null}
+            {(issue.assigned) ? <li className="list-content">
+                {`Assigned: ${issue.assigned}`}
+            </li> : null}
+            {(issue.dueDate) ? <li className="list-content">
+                {`Due Date: ${issue.dueDate}`}
+            </li> : null}
+            {(issue.comments) ? <li className="list-content">
+                {`Comments: ${issue.comments}`}
+            </li> : null}
+
+            {(issue.isOpen === true) ? <li className="list-content">
+                Open
+                </li> : null}
+            {(issue.isOpen === false) ? <li className="list-content">
+                Closed
+                </li> : null}
+
+            {/* Buttons */}
+
+            {(issue.name)
+                ? <button className="link-button" name="deleteIssue" onClick={() => setDisplayDeleteMsg(true)}>
+                    Delete Issue
+                    </button>
+                : null}
+
+            {(displayDeleteMsg)
+                ? <DeleteConfirmation type="issue" onConfirm={handleDeleteIssue} onReject={removeDeleteConfirmation} />
+                : null}
+        </ul>
+    );
+}
+
+export default IssueDetailsList;

af687752e56a28670116f634b0e525f244504dec
nbur4556 <nbur4556@gmail.com>
2021-04-25 20:48:57 -0500

    feat: remove tab from tab bar when respective project is deleted

--- client/src/pages/Workbench/ProjectInterface.js
+++ client/src/pages/Workbench/ProjectInterface.js
@@ -3,6 +3,8 @@ import ApiConnection from '../../utils/ApiConnection.js';
 const projectConnection = new ApiConnection('/api/project');
 
 const ProjectInterface = (props) => {
+    const { dispatch, ACTIONS } = props.uiDispatcher;
+
     const handleEditProject = (e, projectId, projectData) => {
         e.preventDefault();
 
@@ -13,9 +15,11 @@ const ProjectInterface = (props) => {
     const handleDeleteProject = (projectId) => {
         projectConnection.deleteQuery({ urlExtension: `/${projectId}` })
             .then(() => {
-                for (const tab of props.ui.projectTabs) {
-                    console.log(tab._id);
-                }
+                props.ui.projectTabs.forEach((tab, index) => {
+                    (projectId === tab._id)
+                        ? dispatch({ type: ACTIONS.REMOVE_PROJECT_TAB, payload: { tabIndex: index } })
+                        : console.log('not found');
+                })
                 props.handleLoadData()
             });
     }

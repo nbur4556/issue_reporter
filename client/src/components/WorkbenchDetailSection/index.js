d88a9fc2730493d2d61fc66060dd8ea2d561f23c
nbur4556 <nbur4556@gmail.com>
2021-05-04 19:13:41 -0500

    feat: close any workbench detail section when close detail button is clicked

--- client/src/components/WorkbenchDetailSection/index.js
+++ client/src/components/WorkbenchDetailSection/index.js
@@ -9,19 +9,28 @@ import CreateIssue from './CreateIssue.js';
 import IssueDetails from './IssueDetails.js';
 
 // Contexts
-import { UiContext } from '../../pages/Workbench';
+import { UiContext, UiDispatcherContext } from '../../pages/Workbench';
 
 const WorkbenchDetailSection = (props) => {
     const ui = useContext(UiContext);
+    const { dispatch, ACTIONS } = useContext(UiDispatcherContext);
 
     const setMobileVisibility = () => {
         return (ui.displayProjectManager || ui.displayCreateProject || ui.displayCreateIssue || ui.selectIssue)
             ? 'show-on-mobile' : 'hide-on-mobile';
     }
 
+    const closeDetailSection = () => {
+        console.log(props);
+        if (ui.displayProjectManager) { dispatch({ type: ACTIONS.TOGGLE_PROJECT_MANAGER }) }
+        if (ui.displayCreateProject) { dispatch({ type: ACTIONS.TOGGLE_CREATE_PROJECT }) }
+        if (ui.displayCreateIssue) { dispatch({ type: ACTIONS.TOGGLE_CREATE_ISSUE }) }
+        if (ui.selectIssue) { dispatch({ type: ACTIONS.SELECT_ISSUE, payload: { issueId: ui.selectIssue } }) }
+    }
+
     return (
         <section className={`detail-section ${setMobileVisibility()}`}>
-            <IconButton iconName="exit" className="close-detail" />
+            <IconButton iconName="exit" className="close-detail" onClick={closeDetailSection} />
 
             {(ui.displayProjectManager) ? <ProjectManager {...props} /> : null}
             {(ui.displayCreateProject) ? <CreateProject {...props} /> : null}

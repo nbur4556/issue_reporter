361d2afc10eb6f931c4a22be2c7bfe8e2497d0ac
nbur4556 <nbur4556@gmail.com>
2021-05-09 18:46:38 -0500

    test: ui workbench test creates and deletes project on run

--- cypress/integration/ui-workbench.spec.js
+++ cypress/integration/ui-workbench.spec.js
@@ -1,11 +1,26 @@
+const projectName = 'projectName';
+
 describe("Issues", () => {
+    let authToken;
+    let projectId;
+
     beforeEach(() => {
-        cy.login().then(() => {
+        cy.login().then((data) => {
+            authToken = data.body.authToken;
+            return cy.createProject(data.body.authToken, { name: projectName });
+        }).then((data) => {
+            projectId = data.body._id;
             cy.visit('/workbench');
         });
     });
 
+    afterEach(() => {
+        cy.deleteProject(authToken, projectId);
+        cy.logout();
+    });
+
     it('Select Issue', () => {
         console.log('select issue');
+
     });
 });

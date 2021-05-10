cd130d01a37b003e90ec134dedeb92ecabff2c2f
nbur4556 <nbur4556@gmail.com>
2021-05-10 18:21:46 -0500

    style: add initial not found page styling

--- client/src/pages/NotFound/index.js
+++ client/src/pages/NotFound/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
+import './style.css';
+
 const NotFound = () => {
     return (
-        <main>
+        <main className="not-found-page">
             <h1>Error: 404 Not Found</h1>
             <Link to="/">Home</Link>
         </main>
